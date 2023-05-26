0f418758a7f716a875d723b7fa1c847c7ce39fed
galmier forissier <galmierforissier@yahoo.fr>
2023-05-26 15:42:02 +0200

    fini

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists do
+
+  root to: "lists#index"
+  resources :lists, except: :index do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: :destroy
